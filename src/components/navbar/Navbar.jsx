ca3ea0f9d64ec2e4ebb26a792dc0fe6e193c6343
esther-sh-choi <estherchoi.sh@gmail.com>
2022-06-01 14:20:24 -0400

    navbar update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-import namelogo from "../../images/name-logo.png";
 import "./navbar.scss";
 import "../../global.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
@@ -10,11 +13,30 @@ export default function Navbar() {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="name-logo">
-            <p className="logo--first">esther\</p>
+            <p className="logo--first">esther/</p>
             <p className="logo--last">choi</p>
           </a>
+          <div className="item-container">
+            <FontAwesomeIcon icon={faPhone} className="icon phone-icon" />
+            <span>+1-416-729-5604</span>
+          </div>
+          <div className="item-container">
+            <FontAwesomeIcon icon={faEnvelope} className="icon email-icon" />
+            <span>estherchoi.sh@gmail.com</span>
+          </div>
+          <div className="item-container">
+            <a href="http://www.linkedin.com/in/esther--choi">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="icon linkedin-icon"
+              />
+            </a>
+            <span>/esther--choi</span>
+          </div>
         </div>
-        <div className="right">This is right</div>
+        <div className="right">
+          <FontAwesomeIcon icon={faBars} className="icon bars-icon" />
+        </div>
       </div>
     </div>
   );
