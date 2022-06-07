b4dd95ad6cb5eb930c98ab7fb2305008ba0b82ac
esther-sh-choi <estherchoi.sh@gmail.com>
2022-06-07 14:44:21 -0400

    projects section start

---
+++ src/projectsData.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMobileScreen, faDisplay } from "@fortawesome/free-solid-svg-icons";
+
+export const projects = [
+  {
+    icon: [faDisplay, faMobileScreen],
+    id: 0,
+    title: "Ladder Game for Indecisive People",
+    skills: ["Figma", "React.js", "CSS Module"],
+    description:
+      "Howler, Context API, useHistory, useRef, useContext, algorithm, dynamic and responsive design",
+  },
+];
