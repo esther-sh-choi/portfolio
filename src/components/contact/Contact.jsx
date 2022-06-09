8e93eaec04135c18f55dad5bbfb5e445c14f03f6
esther-sh-choi <estherchoi.sh@gmail.com>
2022-06-09 13:09:54 -0400

    begin styling the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.scss";
+import contactImage from "../../images/contact-blue.jpg";
+import Modal from "../UI/Modal";
 
 export default function Contact({ setOpenMenu }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setShowModal(true);
+  };
+
   return (
     <div className="contact" id="contact" onClick={() => setOpenMenu(false)}>
-      Contact
+      <div className="left">
+        <h2>Contact.</h2>
+        <form onSubmit={submitHandler}>
+          <input type="text" placeholder="Email" />
+          <textarea
+            placeholder="Message"
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      <div className="right">
+        <img src={contactImage} alt="" />
+      </div>
+      <Modal />
     </div>
   );
 }
