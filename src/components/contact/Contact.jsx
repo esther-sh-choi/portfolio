695f0674f395e49f9c9e5f56044b5eb6fe0d7e6b
esther-sh-choi <estherchoi.sh@gmail.com>
2022-06-09 15:41:48 -0400

    add success/fail modal and contact section styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import "./contact.scss";
 import contactImage from "../../images/contact-blue.jpg";
 import Modal from "../UI/Modal";
 
 export default function Contact({ setOpenMenu }) {
-  const [showModal, setShowModal] = useState(false);
+  const [messageModal, setMessageModal] = useState();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    setShowModal(true);
+    setMessageModal({
+      title: "Success!",
+      message: "Thank you for reaching out! I will reply to you shortly.",
+    });
+  };
+
+  const closeModalHandler = () => {
+    setMessageModal(null);
   };
 
   return (
-    <div className="contact" id="contact" onClick={() => setOpenMenu(false)}>
-      <div className="left">
-        <h2>Contact.</h2>
-        <form onSubmit={submitHandler}>
-          <input type="text" placeholder="Email" />
-          <textarea
-            placeholder="Message"
-            name="message"
-            id="message"
-            cols="30"
-            rows="10"
-          ></textarea>
-          <button type="submit">Send</button>
-        </form>
+    <Fragment>
+      <div className="contact" id="contact" onClick={() => setOpenMenu(false)}>
+        <div className="left">
+          <h2>Contact.</h2>
+          <form onSubmit={submitHandler}>
+            <input type="text" placeholder="Email" />
+            <textarea
+              placeholder="Message"
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="right">
+          <img src={contactImage} alt="" />
+        </div>
       </div>
-      <div className="right">
-        <img src={contactImage} alt="" />
-      </div>
-      <Modal />
-    </div>
+      {messageModal && (
+        <Modal
+          onConfirm={closeModalHandler}
+          title={messageModal.title}
+          message={messageModal.message}
+        />
+      )}
+    </Fragment>
   );
 }
